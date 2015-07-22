5cba5baf9512050f27ecd995a22fdadc28e9dbd1
Uday Shankar <udaysh99@gmail.com>
2015-07-22 16:04:50 -0400

    described events in README, implemented in server side. all needed SQL statements written

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # SRP-Display
 contains all display and mapping subsystem code
+
+# Client
+Uses a template from google material design as the static material - dynamic content is injected through jQuery.
+Communicates with server for real-time dynamic content using socket.io
+Events emitted:
+client_time - asks for data closest to a selected time
+client_special - special query (will filter so that destruction/creation of data is impossible)
+
+
+# Server
+NodeJS based - serves static content using ExpressJS. Communicates real-time dynamic content to client using socket.io
+Events emitted: 
+data_now - emitted to all sockets each time the most recent data in the database changes (database is checked once per second)
+data_time - emitted to the socket that asks for the data that is closest to a given time
+data_special - emitted to the socket as the response when it asks for a special query
---
+++ server.js
@@ -0,0 +1,98 @@
+var mysql = require("mysql");
+var hash = require("object-hash");
+var express = require("express");
+var app = require("express")();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+var con = mysql.createConnection({
+  host: "10.11.34.174",
+  port: "3306",
+  user: "root",
+  password: "PLATO",
+  database: "shelter_mac"
+});
+
+var schedule;
+
+var cache = {};
+
+var data = {};
+
+con.connect();
+
+data["rows"] = [];
+data["hashCode"] = "";
+
+var queries = {
+	mostRecent: "SELECT * FROM tagreads WHERE readtime = (SELECT MAX(readtime) FROM tagreads);",
+	all: "SELECT * FROM tagreads",
+	atTime: function(unixTime) {
+		return "SELECT * from tagreads where abs(UNIX_TIMESTAMP(readtime) - " + unixTime + ")=(
+				SELECT min(abs(" + unixTime + "-UNIX_TIMESTAMP(readtime))) from tagreads);";
+	}
+};
+
+dataNow();
+schedule = setInterval(dataNow, 1000);
+
+function dataNow()
+{
+	cache = {};
+	con.query(queries.mostRecent, function(err, rows, fields) {
+		if(!err) {
+			cache["rows"] = rows;
+			cache["hashCode"] = hash.sha1(rows);
+			if(cache["hashCode"] != data["hashCode"]) {
+				data = JSON.parse(JSON.stringify(cache));
+				io.emit("data_now", data);
+			}
+		} else {
+			console.log("error getting data from database");
+		}
+	});
+}
+
+function clientSpecial(query, socket) {
+	var ret = {};
+	con.query(query, function(err, rows, fields) {
+		//console.log("called");
+		if(!err) {
+			ret["rows"] = rows;
+			ret["hashCode"] = hash.sha1(rows);
+			console.log("queried");
+		} else {
+			console.log("error getting data from database");
+			ret = null;
+		}
+		socket.emit("data_special", ret);
+	});
+}
+
+function clientTime(time, socket) {
+
+}
+
+io.on('connection', function(socket) {
+
+	console.log("connection received");
+	
+	if(data["hashCode"] != "")
+	{
+		socket.emit("data_now", data);
+	}
+
+	socket.on("client_time", function(msg) {
+		clientTime(msg["time"], socket);
+	});
+
+	socket.on("client_special", function(msg) {
+		clientSpecial(msg["query"], socket);
+	});
+});
+
+app.use(express.static("public"))
+
+http.listen(3000, function(){
+  	console.log('listening on *:3000');
+});
