fb09d923339442631021a439a6d2eaa7a33b672d
Uday Shankar <udaysh99@gmail.com>
2015-07-28 11:14:43 -0400

    working on canvas

--- public/map.js
+++ public/map.js
@@ -7,3 +7,33 @@ function dataNow(msg)
 	// draw the map
 }
 
+function scale()
+{
+	var canvas = document.getElementById('map'),
+	context = canvas.getContext('2d');
+	canvas.width = window.innerWidth;
+	canvas.height = window.innerHeight;
+}
+
+
+
+function test1()
+{
+	scale();
+	$("canvas#map").drawRect({
+		layer: true,
+		draggable: false,
+		fillStyle: "#00ffff",
+		x: 150, y: 100,
+		width: 200,
+		height: 100,
+		click: function(layer)
+		{
+			console.log("called");
+			layer.fillStyle="#f0f000";
+			$("canvas#map").drawLayers();
+		}
+	});
+	
+}
+
